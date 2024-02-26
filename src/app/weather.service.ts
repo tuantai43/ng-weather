5066129e43dce4e374057f3017412e775da1074d
Tài Phạm <taipham@Tais-MacBook-Air.local>
2024-02-26 15:47:52 +0700

    update url

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -12,7 +12,7 @@ export class WeatherService {
   static readonly EXPIRED_TIME = "expired_time";
   static readonly LOCATION = "location";
   static readonly FORECAST = "forecast";
-  static readonly URL = "http://api.openweathermap.org/data/2.5";
+  static readonly URL = "https://api.openweathermap.org/data/2.5";
   static readonly APPID = "5a4b2d457ecbef9eb2a71e480b947604";
   static readonly ICON_URL = "https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/";
   private currentConditions: ConditionsAndZip[] = [];
